e1630cabdaddbaeb129f222e7673f0c8fe2b3560
Siddharth Mittal <smittal6@gmail.com>
2017-12-25 02:10:40 +0530

    add dummy file

--- models/dummy.py
+++ models/dummy.py
@@ -1,14 +1,35 @@
-import numpy
-strokes = numpy.load('../data/strokes.npy')
+import numpy as np
+import torch
+import torch.nn as nn
+from models.model import UnconditionedHand
+from models.con_model import ConditionedHand
+from utils.dataloader import StrokesDataset
+
+strokes = np.load('../data/strokes.npy')
 stroke = strokes[0]
+
+
+
+def get_testinput():
+    start_stroke = np.asarray((0,0,0))
+    start = torch.from_numpy(start_stroke).float()
+    start = Variable(start)
+    start = start.view(-1,1,3)
+    return start
+
 
 
 def generate_unconditionally(random_seed=1):
     # Input:
     #   random_seed - integer
-
     # Output:
     #   stroke - numpy 2D-array (T x 3)
+    dataset = StrokesDataset()
+    mod = UnconditionedHand()
+    mod.load_state_dict(torch.load('./save/uncon.model'))
+    input = get_testinput()
+    stroke = mod.get_stroke(input)
+    stroke = stroke.astype(np.float32)
     return stroke
 
 
@@ -19,6 +40,8 @@ def generate_conditionally(text='welcome to lyrebird', random_seed=1):
 
     # Output:
     #   stroke - numpy 2D-array (T x 3)
+    dataset = StrokesDataset()
+    mod = ConditionedHand()
     return stroke
 
 
